874119a8a33e790db44a95c0d0d133dc317d690f
ahc2806 <alonsohernandezcortez@gmail.com>
2021-03-14 20:15:06 -0600

    modelo de reportes exportado

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,8 +1,9 @@
 export { default as Auth } from './auth';
 export { default as Diagnostics } from './diagnostics';
+export { default as Machine } from './machine';
 export { default as Person } from './person';
 export { default as Person_Canalize } from './person_canalize';
+export { default as Report } from './report';
 export { default as Role } from './role';
 export { default as Student } from './student';
 export { default as User } from './user';
-export { default as Machine } from './machine';
